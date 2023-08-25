ce36178e25580b83e4e96fcb0afb65fa482ce4c3
vipin kumar gusain <vipin143gusain@gmail.com>
2023-08-25 01:39:10 +0000

    operations in datatypes

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -24,3 +24,27 @@ let someNumber = 33
 let stringNumber = String (someNumber)
 console. log (stringNumber);
 console. log (typeof stringNumber);
+
+
+// ******** Operations *********
+let value = 3
+let negValue = -value
+// console. log (negValue);
+
+let str1 = "hello"
+let str2 = " hitesh"
+let str3 = str1 + str2
+console. log (str3);
+
+console. log ("" + 2);
+console. log (1 + "2");
+console. log ("1" + 2 + 2);
+console. log (1 + 2 + "2");
+console. log ( (3 + 4) * 5 % 3);
+
+let gameCounter = 100
+++gameCounter;
+console. log (gameCounter);
+
+// link to study
+//https://tc39.es/ecma262/multipage/abstract-operations.html#sec-type-conversion
