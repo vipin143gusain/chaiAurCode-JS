cf8a1371789b6283b5306e803b479fbc11531ce7
vipin kumar gusain <vipin143gusain@gmail.com>
2023-08-25 00:53:39 +0000

    variables and data types

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,14 @@
+console.log("vipin")
+
+const accountId = 12345;
+var accountEmail = "v@gmail.com"
+let accountPassword = 3232;
+accountCiyt = "delhi"
+
+
+// accountId=43434;
+accountEmail="vipin@gmail.co";
+accountPassword=8888;
+accountCiyt= "bengaluru"
+
+console.table([ accountEmail, accountPassword, accountCiyt]);
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,13 @@
+let state;
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined =>
+// symbol => unique
+// obiect
+
+
+console. log (typeof undefined); // undefined
+console. log (typeof null); // object
