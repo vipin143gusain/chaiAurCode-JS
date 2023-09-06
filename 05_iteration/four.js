cf5daf83692acf4639fc9849541bf605f46475a9
vipin kumar gusain <vipin143gusain@gmail.com>
2023-09-06 14:24:47 +0000

    loops in javacript

---
+++ 05_iteration/four.js
@@ -0,0 +1,15 @@
+let obj= {
+    name:"vipin", age:26, profession: "software engg."
+}
+
+for (const key in obj) {
+    // console.log(`${key} = ${obj[key]}`);
+   
+}
+
+let myArray = [1,2,3,4,5,6,70]
+
+for (const key in myArray) {
+    console.log(myArray[key])
+   
+}
