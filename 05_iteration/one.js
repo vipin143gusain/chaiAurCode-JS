067a6e4279cb3a9108420857bc4b1a02624d704b
vipin kumar gusain <vipin143gusain@gmail.com>
2023-09-06 00:19:21 +0000

    loops in js

---
+++ 05_iteration/one.js
@@ -0,0 +1,72 @@
+// for
+
+for (let i = 0; i <= 10; i++) {
+    const element = i;
+    if (element == 5) {
+        //console.log("5 is best number");
+    }
+    //console.log(element);
+    
+}
+
+// console.log(element);
+
+for (let i = 1; i <= 10; i++) {
+    //console.log(`Outer loop value: ${i}`);
+   for (let j = 1; j <= 10; j++) {
+    //console.log(`Inner loop value ${j} and inner loop ${i}`);
+    //console.log(i + '*' + j + ' = ' + i*j );
+   }
+    
+}
+let myArray = ["flash", "batman", "superman"]
+//console.log(myArray.length);
+for (let index = 0; index < myArray.length; index++) {
+    const element = myArray[index];
+    //console.log(element);
+    
+}
+
+
+// break and continue
+
+// for (let index = 1; index <= 20; index++) {
+//     if (index == 5) {
+//         console.log(`Detected 5`);
+//         break
+//     }
+//    console.log(`Value of i is ${index}`);
+    
+// }
+
+for (let index = 1; index <= 20; index++) {
+    if (index == 5) {
+        // console.log(`Detected 5`);
+        continue
+    }
+//    console.log(`Value of i is ${index}`);
+    
+}
+
+
+for (let i = 1; i <= 10; i++) {
+   
+    // console.log("Outer loop");
+
+    for (let index = 1; index <= 10; index++) {
+        const element = i*index;
+        // console.log(`${i} X ${index} = ${element}`);
+        
+    }
+    
+}
+
+
+const myArray1 = ["vIPIN","AAIRA","MEENA"]
+
+for (let index = 0; index < myArray1.length; index++) {
+    const element = myArray1[index];
+    const resule = element.includes('AAIRA')
+    console.log(resule);
+    
+}
